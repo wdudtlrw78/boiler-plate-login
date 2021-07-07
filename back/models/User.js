7ee0d13e083e1f0d932df31e24055dfd9d9cc08c
momo <wdudtlrw78@gmail.com>
2021-07-07 22:08:19 +0900

    Add: Bcrypt

--- back/models/User.js
+++ back/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -34,6 +37,24 @@ const userSchema = mongoose.Schema({
     // 토큰 유효 기간
     type: Number,
   },
+});
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model('User', userSchema);
